3b3a310efa638997357a30295f38db0f73f6e641
kikuchimiu <miu.kikuchi@rakus-partners.co.jp>
2021-11-11 12:11:15 +0900

    q1

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -1,11 +1,13 @@
 package junit.tutorial.ex01.e01;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringUtilsTest {
@@ -27,8 +29,27 @@ class StringUtilsTest {
 	}
 
 	@Test
-	void testToSnakeCase() {
-		fail("Not yet implemented");
+	@DisplayName("「aaa」を入力すると「aaa」が取得できる")
+	void test1() {
+		String result = StringUtils.toSnakeCase("aaa");
+		assertEquals("aaa", result, "aaa_error");
+		
 	}
+	@Test
+	@DisplayName("「HelloWorld」を入力すると「hello_world」が取得できる")
+	void test2() {
+		String result = StringUtils.toSnakeCase("HelloWorld");
+		assertEquals("hello_world", result, "HelloWorld_error");
+		
+	}
+	@Test
+	@DisplayName("「practiceJunit」を入力すると「practice_junit」が取得できる")
+	void test3() {
+		String result = StringUtils.toSnakeCase("practiceJunit");
+		assertEquals("practice_junit", result, "practice_junit_error");
+		
+	}
+	
+	
 
 }
